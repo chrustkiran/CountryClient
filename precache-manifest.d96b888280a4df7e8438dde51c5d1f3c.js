5b0fc96227bf835c329021a2c64a2b77f46c445e
chrustkiran <christkiran.15@cse.mrt.ac.lk>
2020-04-08 20:30:34 +0530

    Updates

---
+++ precache-manifest.d96b888280a4df7e8438dde51c5d1f3c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d2834b5dc181787a238cf292dcd347b",
+    "url": "/CountryClient/index.html"
+  },
+  {
+    "revision": "95a4b63b510512faef5c",
+    "url": "/CountryClient/static/css/2.ad8fe80d.chunk.css"
+  },
+  {
+    "revision": "746ed974a6e75171b837",
+    "url": "/CountryClient/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "95a4b63b510512faef5c",
+    "url": "/CountryClient/static/js/2.eb840280.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/CountryClient/static/js/2.eb840280.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "746ed974a6e75171b837",
+    "url": "/CountryClient/static/js/main.794f8439.chunk.js"
+  },
+  {
+    "revision": "77f4f3a66a96bdedfb20",
+    "url": "/CountryClient/static/js/runtime-main.a023bb08.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/CountryClient/static/media/logo.5d5d9eef.svg"
+  }
+]);
